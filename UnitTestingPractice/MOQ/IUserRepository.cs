fc098a09248ceef1b393a4e44b5d9461304d1d50
vishalkgit <kalevishal1012@gmail.com>
2026-03-09 23:36:04 +0530

    Practice of MOQ

---
+++ UnitTestingPractice/MOQ/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingPractice.Model;
+
+namespace UnitTestingPractice.MOQ
+{
+    public interface IUserRepository
+    {
+
+        User GetUserById(int id);
+
+        int Add(int a, int b);
+
+        int Sub(int a, int b);
+
+        int Mul(int a, int b);
+    }
+}
